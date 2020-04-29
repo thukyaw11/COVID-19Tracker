9f7d93a37a7b2efa3f73fd130fb9fe8acc51ed33
Hein Thanth <official.heinthanth@gmail.com>
2020-04-29 20:22:42 +0630

    Fix SW

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,32 +1,37 @@
 /* eslint-disable no-console */
 
-import { register } from 'register-service-worker'
+import { register } from "register-service-worker";
 
-if (process.env.NODE_ENV === 'production') {
-  register(`${process.env.BASE_URL}service-worker.js`, {
-    ready() {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      )
-    },
-    registered() {
-      console.log('Service worker has been registered.')
-    },
-    cached() {
-      console.log('Content has been cached for offline use.')
-    },
-    updatefound() {
-      console.log('New content is downloading.')
-    },
-    updated() {
-      console.log('New content is available; Refresh...')
-    },
-    offline() {
-      console.log('No internet connection found. App is running in offline mode.')
-    },
-    error(error) {
-      console.error('Error during service worker registration:', error)
-    }
-  })
+if (process.env.NODE_ENV === "production") {
+	register(`${process.env.BASE_URL}service-worker.js`, {
+		ready() {
+			console.log(
+				"App is being served from cache by a service worker.\n" +
+					"For more details, visit https://goo.gl/AFskqB"
+			);
+		},
+		registered() {
+			console.log("Service worker has been registered.");
+		},
+		cached() {
+			console.log("Content has been cached for offline use.");
+		},
+		updatefound() {
+			console.log("New content is downloading.");
+		},
+		updated() {
+			console.alert("New webapp build is available; refreshing ...");
+			setTimeout(() => {
+				window.location.reload(true);
+			}, 1000);
+		},
+		offline() {
+			console.log(
+				"No internet connection found. App is running in offline mode."
+			);
+		},
+		error(error) {
+			console.error("Error during service worker registration:", error);
+		},
+	});
 }
